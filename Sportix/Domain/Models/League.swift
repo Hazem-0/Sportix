7b235e725cecf97b2d693b7b593aaf09ecf9b4a4
Hazem <hazemkora660@gmail.com>
2026-05-06 21:42:55 +0300

    feat: set up CoreData and implement Sports repository pattern
    setup Domain layer with repo and models
    Add League and Sport Models
    Add repo protocol with fav metholds
    Add FavLeague Entity in core data
    Create CoreDataManager
    Implement Save and remove methods

---
+++ Sportix/Domain/Models/League.swift
@@ -0,0 +1,16 @@
+//
+//  League.swift
+//  Sportix
+//
+//  Created by Hazem Abdelraouf on 06/05/2026.
+//
+
+import Foundation
+
+struct League{
+    let id: Int
+    let name: String
+    let sport: Sport
+    let country: String
+    let badge: String 
+}
